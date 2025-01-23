e5854b44ee3e3ef33dcd1689bb47aa8051b425e4
alin <herciualin10@gmail.com>
2025-01-23 10:01:00 +0100

    initialized spring api

---
+++ selsolun-back/src/main/java/fr/doranco/selsolunback/SelsolunBackApplication.java
@@ -0,0 +1,13 @@
+package fr.doranco.selsolunback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelsolunBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelsolunBackApplication.class, args);
+    }
+
+}
