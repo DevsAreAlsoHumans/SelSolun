82bb0dda64e82ef91586e36ba284561487a8e57a
TheSpaceRangers <tronel.charles@gmail.com>
2025-01-23 23:29:43 +0100

    Refactor CORS configuration into SecurityConfiguration.

--- solsolun-back/src/main/java/fr/doranco/solsolunback/config/SecurityConfiguration.java
+++ solsolun-back/src/main/java/fr/doranco/solsolunback/config/SecurityConfiguration.java
@@ -3,9 +3,11 @@ package fr.doranco.solsolunback.config;
 import fr.doranco.solsolunback.components.JWTAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,6 +15,11 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -25,12 +32,15 @@ public class SecurityConfiguration {
     private final JWTAuthenticationFilter jwtAuthenticationFilter;
     private final AuthenticationProvider authenticationProvider;
 
+    @Value("#{'${app.security.allowed-origins:}'.split(',')}")
+    private List<String> allowedOrigins;
+
     @Bean
     public DefaultSecurityFilterChain defaultSecurityFilterChain(
             HttpSecurity http
     ) throws Exception {
         http
-                .cors(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
@@ -42,5 +52,28 @@ public class SecurityConfiguration {
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource createCorsConfigSource() {
+        System.out.println(allowedOrigins);
+
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        //config.addAllowedOrigin("http://localhost:4200");
+        //config.addAllowedOrigin("http://localhost:4200");
+        this.allowedOrigins.forEach(config::addAllowedOrigin);
+
+        config.setAllowedHeaders(List.of("*"));
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("DELETE");
+        config.setAllowCredentials(true);
+        config.setMaxAge(3600L);
+
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 }
